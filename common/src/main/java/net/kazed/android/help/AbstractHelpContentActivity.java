61ac8a296b2e745d45d5ff95dc172c26a4ccba5b
koert zeilstra <koert.zeilstra@gmail.com>
2011-03-24 19:07:46 +0100

    Incomplete - task : Localized help texts

--- common/src/main/java/net/kazed/android/help/AbstractHelpContentActivity.java
+++ common/src/main/java/net/kazed/android/help/AbstractHelpContentActivity.java
@@ -3,16 +3,22 @@ package net.kazed.android.help;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public abstract class AbstractHelpContentActivity extends Activity {
 
@@ -20,8 +26,9 @@ public abstract class AbstractHelpContentActivity extends Activity {
    
    private WebView webView;
    private Uri itemUri;
-   private String helpFile;
-   private boolean useLanguage;
+   private String currentHelpFile;
+
+   private AssetManager assetManager;
 
    protected abstract int getContentViewId();
 
@@ -32,10 +39,13 @@ public abstract class AbstractHelpContentActivity extends Activity {
       super.onCreate(icicle);
       itemUri = getIntent().getData();
       if (itemUri == null) {
-         helpFile = "welcome.html";
-      } else {
-         helpFile = itemUri.getLastPathSegment();
+         currentHelpFile = "welcome.html";
+      } else { 
+         currentHelpFile = itemUri.getLastPathSegment();
       }
+      Log.d("help", "onCreate " + itemUri + " - " + currentHelpFile);
+
+      assetManager = getAssets();
 
       setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
       requestWindowFeature(Window.FEATURE_PROGRESS);
@@ -43,42 +53,102 @@ public abstract class AbstractHelpContentActivity extends Activity {
 
       webView = (WebView) findViewById(getWebkitViewId());
       webView.setWebViewClient(new WebViewClient() {
-         public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            if (failingUrl.endsWith(helpFile)) {
-               if (useLanguage) {
-                  displayDefaultFile(helpFile);
-               }
-            } else {
-               if (failingUrl.startsWith(FILE_URL_PREFIX)) {
-                  int index = failingUrl.lastIndexOf('/');
-                  if (index >= 0) {
-                     helpFile = failingUrl.substring(index+1);
-                     webView.loadUrl(failingUrl);
-                  }
-               } else {
+         private String failedUrl;
+         
+         @Override
+         public void onPageFinished(final WebView view, final String url) {
+            Log.d("help", "onPageFinished " + url);
+            super.onPageFinished(view, url);
+            if (failedUrl != null) {
+               if (!failedUrl.startsWith(FILE_URL_PREFIX)) {
                   // create intent for web browser
+                  webView.goBack();
+                  Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(failedUrl));
+                  startActivity(intent);
                }
+               failedUrl = null;
             }
+
+         }
+
+         public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Log.d("help", "onReceivedError " + failingUrl);
+            failedUrl = failingUrl;
+
+//            failedUrls.add(failingUrl);
+//            if (visitedUrls.size() > 0) {
+//               String lastUrl = visitedUrls.get(visitedUrls.size() - 1);
+//               if (failingUrl.equals(lastUrl)) {
+//                  visitedUrls.remove(visitedUrls.size() - 1);
+//                  Log.d("help", "onReceivedError removed " + visitedUrls);
+//               }
+//            }
+//            if (failingUrl.startsWith(FILE_URL_PREFIX)) {
+//               int lastSlashIndex = failingUrl.lastIndexOf('/');
+//               String helpFile = failingUrl.substring(lastSlashIndex+1);
+//               boolean containsLanguage = (lastSlashIndex > FILE_URL_PREFIX.length());
+//               if (containsLanguage) {
+//                  webView.goBack();
+//                  displayDefaultFile(helpFile);
+//               }
+//            } else {
+//               // create intent for web browser
+////               webView.goBack();
+//               Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(failingUrl));
+//               startActivity(intent);
+//            }
+            
+//            if (failingUrl.endsWith(currentHelpFile)) {
+//               if (useLanguage) {
+//                  displayDefaultFile(currentHelpFile);
+//               }
+//            } else {
+//               if (failingUrl.startsWith(FILE_URL_PREFIX)) {
+//                  int index = failingUrl.lastIndexOf('/');
+//                  if (index >= 0) {
+//                     currentHelpFile = failingUrl.substring(index+1);
+//                     webView.loadUrl(failingUrl);
+//                  }
+//               } else {
+//                  // create intent for web browser
+//               }
+//            }
          }
        });
 
+      String language = getLanguage();
+      displayFile(language, currentHelpFile);
    }
 
    @Override
    protected void onResume() {
       super.onResume();
+      String language = getLanguage();
+      displayFile(language, currentHelpFile);
+   }
+
+   private String getLanguage() {
       String language = getResources().getConfiguration().locale.getLanguage();
-      displayFile(language, helpFile);
+      return language;
    }
 
    private void displayFile(final String language, final String helpFile) {
-      if (language == null) {
-         useLanguage = false;
-         webView.loadUrl(FILE_URL_PREFIX + helpFile);
-      } else {
-         useLanguage = true;
-         webView.loadUrl(FILE_URL_PREFIX + language + "/" + helpFile);
+      boolean useLanguage = false;
+      if (language != null) {
+         try {
+            assetManager.open(language + "/" + helpFile);
+            useLanguage = true;
+         } catch (IOException e) {
+            useLanguage = false;
+         }
+         
       }
+      if (useLanguage) {
+         webView.loadUrl(FILE_URL_PREFIX + language + "/" + helpFile);
+      } else {
+         webView.loadUrl(FILE_URL_PREFIX + helpFile);
+      }
+      currentHelpFile = helpFile;
    }
 
    private void displayDefaultFile(final String helpFile) {
@@ -93,15 +163,30 @@ public abstract class AbstractHelpContentActivity extends Activity {
       } else {
          result = super.onKeyDown(keyCode, event);
       }
+
+//      Log.d("help", "onKeyDown " + keyCode + " - " + visitedUrls);
+//      if (KeyEvent.KEYCODE_BACK == keyCode && visitedUrls.size() > 1) {
+//         String lastUrl = visitedUrls.get(visitedUrls.size() - 2);
+//         int lastSlashIndex = lastUrl.lastIndexOf('/');
+//         currentHelpFile = lastUrl.substring(lastSlashIndex+1);
+//         visitedUrls.remove(visitedUrls.size() - 2);
+//         displayFile(getLanguage(), currentHelpFile);
+//      } else {
+//         result = super.onKeyDown(keyCode, event);
+//      }
       return result;
    }
 
    @Override
    public boolean onKeyUp(int keyCode, KeyEvent event) {
       boolean result = true;
-      if (KeyEvent.KEYCODE_BACK == keyCode && webView.canGoBack()) {
-         webView.goBack();
-      } else {
+//      if (KeyEvent.KEYCODE_BACK == keyCode && webView.canGoBack()) {
+//         webView.goBack();
+//         String url = webView.getOriginalUrl();
+//         int lastSlashIndex = url.lastIndexOf('/');
+//         currentHelpFile = url.substring(lastSlashIndex+1);
+//      } else {
+      if (KeyEvent.KEYCODE_BACK != keyCode) {
          result = super.onKeyUp(keyCode, event);
       }
       return result;
